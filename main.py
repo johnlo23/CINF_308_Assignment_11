ae14bd4cfb5efa21b6860e3b9a4fd3e9d8a38402
john <john@jlny.org>
2022-10-27 15:31:48 -0400

    13.2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # John Logiudice
 # INF 308 - Fall 2022
 # Assignment 11 - Case Study - Data Structures
-# 13.1
+# 13.2
 
 # Make string library available
 from string import *
+
+FILE_NAME = 'MobyDick.txt'
 
 
 # Instantiate a word and clean word of extra characters
@@ -58,7 +60,7 @@ class Word:
 class Line:
 
     def __init__(self, line=None):
-        
+
         if line is None:
             self.line = ''
         else:
@@ -69,6 +71,7 @@ class Line:
         # Get word list every time a Line is instantiated
         self.get_words()
 
+    # Extract single words from a line
     def get_words(self):
         # Convert entire line to lower case
         line = self.line.lower()
@@ -77,35 +80,62 @@ class Line:
         self.words = line.replace('—', ' ').split()
 
 
+# Instantiate a book, open the file and do analysis
 class Ebook:
 
     def __init__(self, filename):
         self.filename = filename
+        self.word_count = None
+        self.unique_count = None
 
         # Initialize empty word dictionary
         self.word_dict = {}
 
+    # Open a book text file and analyse the contents
     def read_lines(self):
+        # Initialize counter
+        count = 0
+        # Initialize in header variable
+        in_header_footer = True
         # open the file for reading
         with open(self.filename, encoding="utf-8") as file:
             # Iterate over each line
             for line in file:
                 new_line = Line(line)
-                # Extract words from the line
-                words = new_line.words
-                # Iterate over each word
-                for word in words:
-                    new_word = Word(word)
-                    # Use clean method to get each word
-                    new_word = new_word.clean_word()
-                    # ignore words that were returned as None
-                    if new_word is not None:
-                        print(new_word)
+                # If in header or footer, do not process lines
+                if not in_header_footer:
+                    # Extract words from the line
+                    words = new_line.words
+                    # Iterate over each word
+                    for word in words:
+                        new_word = Word(word)
+                        # Use clean method to get each word
+                        new_word = new_word.clean_word()
+                        # ignore words that were returned as None
+                        if new_word is not None:
+                            # Add word to dictionary and update the count
+                            self.word_dict[new_word] = self.word_dict.get(new_word, 0) + 1
+
+                            # Increment total count
+                            count += 1
+
+                # Check for header and footer start and end
+                if line.startswith("*** START"):
+                    in_header_footer = False
+                elif line.startswith("*** END"):
+                    in_header_footer = True
+
+        # Only update the count variables if counts were done
+        if count > 0:
+            self.word_count = count
+            self.unique_count = len(self.word_dict)
 
 
 def main():
-    ebook = Ebook("sample.txt")
+    ebook = Ebook(FILE_NAME)
     ebook.read_lines()
+    print(f"The total number of words in the book: {ebook.word_count}")
+    print(f"The number of different words in the book: {ebook.unique_count}")
 
 
 if __name__ == '__main__':
